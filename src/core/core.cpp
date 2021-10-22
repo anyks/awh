b85edd81bc6b96e9b659918413b8e6f0c85bbbcc
Forman <forman@anyks.com>
2021-10-22 16:36:58 +0300

    Отладка получения MAC адреса

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -438,7 +438,7 @@ void awh::Core::start() noexcept {
 
 		for(auto & item : hw) cout << " ==== " << item.first << " == " << item.second << endl;
 
-		cout << " ++++++++= " << net.ip("enp4s0", AF_INET) << endl;
+		cout << " ++++++++= " << net.ip("vtnet0", AF_INET) << endl;
 
 
 		// Разрешаем работу WebSocket
