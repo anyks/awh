f967912b94389d5c314dae4fd2f0da2c63a1a3b5
Forman <forman@anyks.com>
2021-10-26 23:40:50 +0300

    Реализация поддержки работы с MAC адресами в Linux

--- src/server/core.cpp
+++ src/server/core.cpp
@@ -188,7 +188,7 @@ void awh::CoreServer::accept(const evutil_socket_t fd, const short event, void *
 		// 2a03:b0c0:3:d0::42e1:c001
 		// 2400:6180:0:d0::70:7001
 		// const string mac1 = core->ifnet.mac("192.168.1.2");//, AF_INET6);
-		const string mac1 = core->ifnet.mac("fe80::c4fa:73ff:fef8:ad9b", AF_INET6);
+		const string mac1 = core->ifnet.mac("2604:a880:400:d0::1", AF_INET6);
 		// const string mac1 = core->ifnet.mac("2400:6180:0:d0::70:7001", AF_INET6);
 		cout << " *************** " << mac1 << endl;
 		
