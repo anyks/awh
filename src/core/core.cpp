cf5a8847ac3672cc1ea53c0435ca5bae25835308
Юрий Лобарев <forman@anyks.com>
2022-07-27 15:27:52 +0300

    Отлаживаем работу с UnixSocket

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1021,7 +1021,7 @@ void awh::Core::rebase() noexcept {
 		 */
 		#if defined(_WIN32) || defined(_WIN64)
 			// Создаем новую базу
-			this->base = ev_default_loop(0);
+			this->base = ev_loop_new(ev_recommended_backends ());
 		/**
 		 * Если операционной системой является Linux
 		 */
