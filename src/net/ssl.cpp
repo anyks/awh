91df341ad1114a2dade8d294d65e04c224930309
Юрий Лобарев <forman@anyks.com>
2022-07-30 12:55:17 +0300

    Выполняем отладку сервера

--- src/net/ssl.cpp
+++ src/net/ssl.cpp
@@ -402,11 +402,11 @@ awh::ASSL::ctx_t awh::ASSL::init() noexcept {
 		const int cert = (!this->cert.empty() ? SSL_CTX_use_certificate_file(result.ctx, this->cert.c_str(), SSL_FILETYPE_PEM) : 0);
 		*/
 
-		
+		/*
 		cout << " -----------------------1 " << result.ctx << " === " << this->cert << " === " << SSL_CTX_use_certificate_file(result.ctx, this->cert.c_str(), SSL_FILETYPE_PEM) << endl;
 		cout << " -----------------------2 " << result.ctx << " === " << this->key << " === " << SSL_CTX_use_RSAPrivateKey_file(result.ctx, this->key.c_str(), SSL_FILETYPE_PEM) << endl;
 		cout << " -----------------------3 " << result.ctx << " === " << this->chain << " === " << SSL_CTX_use_certificate_chain_file(result.ctx, this->chain.c_str()) << endl;
-		
+		*/
 		
 		/*
 		int SSL_use_certificate_file(SSL *ssl, const char *file, int type);
@@ -430,11 +430,11 @@ awh::ASSL::ctx_t awh::ASSL::init() noexcept {
 		// Создаем ssl объект
 		result.ssl = SSL_new(result.ctx);
 
-		/*
+		
 		cout << " =======================1 " << result.ssl << " === " << this->cert << " === " << SSL_use_certificate_file(result.ssl, this->cert.c_str(), SSL_FILETYPE_PEM) << endl;
 		cout << " =======================2 " << result.ssl << " === " << this->key << " === " << SSL_use_PrivateKey_file(result.ssl, this->key.c_str(), SSL_FILETYPE_PEM) << endl;
 		cout << " =======================3 " << result.ssl << " === " << this->chain << " === " << SSL_use_certificate_chain_file(result.ssl, this->chain.c_str()) << endl;
-		*/
+		
 
 		// Если объект не создан
 		if(!(result.mode = (result.ssl != nullptr))){
