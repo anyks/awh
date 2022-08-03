2f19b595e829650ad3f5a708e1bbba15315af922
Юрий Лобарев <forman@anyks.com>
2022-08-03 12:30:09 +0300

    Выполняем чистку кода

--- sample/wss.cpp
+++ sample/wss.cpp
@@ -158,17 +158,15 @@ int main(int argc, char * argv[]){
 	// Устанавливаем тип авторизации
 	// ws.setAuthType(auth_t::type_t::DIGEST, auth_t::hash_t::SHA256);
 	// Выполняем инициализацию WebSocket сервера
-	ws.init(2222, "127.0.0.1", http_t::compress_t::DEFLATE);
-	// ws.init(2222, "", http_t::compress_t::DEFLATE);
+	// ws.init(2222, "127.0.0.1", http_t::compress_t::DEFLATE);
+	ws.init(2222, "", http_t::compress_t::DEFLATE);
 	// ws.init(2222, "127.0.0.1", http_t::compress_t::NONE);
 	// ws.init("anyks", http_t::compress_t::DEFLATE);
-	/*
 	// Устанавливаем SSL сертификаты сервера
 	core.setCert(
 		"/usr/local/etc/letsencrypt/live/anyks.net/fullchain.pem",
 		"/usr/local/etc/letsencrypt/live/anyks.net/privkey.pem"
 	);
-	*/
 	// Устанавливаем шифрование
 	// ws.setCrypt("PASS");
 	// Устанавливаем сабпротоколы
