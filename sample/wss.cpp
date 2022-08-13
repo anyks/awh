4cb8255fc3451045353cc4f8f1ea89dd1685d260
Юрий Лобарев <forman@anyks.com>
2022-08-13 20:11:30 +0300

    Отлаживаем работу SCTP

--- sample/wss.cpp
+++ sample/wss.cpp
@@ -147,11 +147,11 @@ int main(int argc, char * argv[]){
 	// Устанавливаем тип сокета unix-сокет
 	// core.family(core_t::family_t::NIX);
 	// Устанавливаем тип сокета UDP TLS
-	// core.sonet(core_t::sonet_t::DTLS);
+	core.sonet(core_t::sonet_t::DTLS);
 	// core.sonet(core_t::sonet_t::TLS);
 	// core.sonet(core_t::sonet_t::UDP);
 	// core.sonet(core_t::sonet_t::TCP);
-	core.sonet(core_t::sonet_t::SCTP);
+	// core.sonet(core_t::sonet_t::SCTP);
 
 	// Отключаем валидацию сертификата
 	core.verifySSL(false);
