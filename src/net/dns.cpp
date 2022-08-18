1d94c529dc687ef49ff784f7293dad3ac8e6cda2
Юрий Лобарев <forman@anyks.com>
2022-08-18 16:59:51 +0300

    Дорабатываем DNS резолвер

--- src/net/dns.cpp
+++ src/net/dns.cpp
@@ -485,7 +485,7 @@ bool awh::DNS::Worker::request(const string & domain) noexcept {
 					header->qr = 0;
 					header->aa = 0;
 					header->tc = 0;
-					header->rd = 2;
+					header->rd = 0;
 					header->ra = 0;
 					header->rcode = 0;
 					header->opcode = 0;
