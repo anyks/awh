69c6fb31a6c6227aa8069a1a35da23d5a45d8d76
Лобарев Юрий <forman@anyks.com>
2023-10-05 14:49:48 +0300

    Отлаживаем работу WebSocket-сервера

--- src/net/engine.cpp
+++ src/net/engine.cpp
@@ -3300,11 +3300,9 @@ void awh::Engine::wrapServer(ctx_t & target, addr_t * address) noexcept {
 			// Запускаем кэширование
 			SSL_CTX_set_session_cache_mode(target._ctx, SSL_SESS_CACHE_SERVER | SSL_SESS_CACHE_NO_INTERNAL);
 			*/
+
 			// Если цепочка сертификатов установлена
 			if(!this->_chain.empty()){
-				
-				cout << " ^^^^^^^^^^^^^^^^^ " << this->_chain << endl;
-				
 				// Если цепочка сертификатов не установлена
 				if(SSL_CTX_use_certificate_chain_file(target._ctx, this->_chain.c_str()) < 1){
 					// Выводим в лог сообщение
@@ -3782,6 +3780,7 @@ awh::Engine::Engine(const fmk_t * fmk, const log_t * log, const uri_t * uri) noe
 	// Выполняем модификацию доверенного сертификата (CA-файла)
 	this->_ca = this->_fs.realPath(this->_ca);
 	// Выполняем установку алгоритмов шифрования
+	/*
 	this->ciphers({
 		"ECDHE+AESGCM",
 		"ECDHE+CHACHA20",
@@ -3829,6 +3828,52 @@ awh::Engine::Engine(const fmk_t * fmk, const log_t * log, const uri_t * uri) noe
 		"!EDH-RSA-DES-CBC3-SHA",
 		"!KRB5-DES-CBC3-SHA"
 	});
+	*/
+
+	this->ciphers({
+		"ECDHE-RSA-AES128-GCM-SHA256",
+		"ECDHE-ECDSA-AES128-GCM-SHA256",
+		"ECDHE-RSA-AES256-GCM-SHA384",
+		"ECDHE-ECDSA-AES256-GCM-SHA384",
+		"DHE-RSA-AES128-GCM-SHA256",
+		"DHE-DSS-AES128-GCM-SHA256",
+		"kEDH+AESGCM",
+		"ECDHE-RSA-AES128-SHA256",
+		"ECDHE-ECDSA-AES128-SHA256",
+		"ECDHE-RSA-AES128-SHA",
+		"ECDHE-ECDSA-AES128-SHA",
+		"ECDHE-RSA-AES256-SHA384",
+		"ECDHE-ECDSA-AES256-SHA384",
+		"ECDHE-RSA-AES256-SHA",
+		"ECDHE-ECDSA-AES256-SHA",
+		"DHE-RSA-AES128-SHA256",
+		"DHE-RSA-AES128-SHA",
+		"DHE-DSS-AES128-SHA256",
+		"DHE-RSA-AES256-SHA256",
+		"DHE-DSS-AES256-SHA",
+		"DHE-RSA-AES256-SHA",
+		"AES128-GCM-SHA256",
+		"AES256-GCM-SHA384",
+		"AES128-SHA256",
+		"AES256-SHA256",
+		"AES128-SHA",
+		"AES256-SHA",
+		"AES",
+		"CAMELLIA",
+		"DES-CBC3-SHA",
+		"!aNULL",
+		"!eNULL",
+		"!EXPORT",
+		"!DES",
+		"!RC4",
+		"!MD5",
+		"!PSK",
+		"!aECDH",
+		"!EDH-DSS-DES-CBC3-SHA",
+		"!EDH-RSA-DES-CBC3-SHA",
+		"!KRB5-DES-CBC3-SHA"
+	});
+
 	/**
 	 * Если версия OPENSSL ниже версии 1.1.0
 	 */
