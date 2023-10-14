feb7c902c633304bb9abc00ea5dae3164a995304
Лобарев Юрий <forman@anyks.com>
2023-10-15 02:07:51 +0300

    Отлаживаем работу мультисервера HTTP/2

--- src/server/web/http2.cpp
+++ src/server/web/http2.cpp
@@ -739,10 +739,6 @@ void awh::server::Http2::prepare(const int32_t sid, const uint64_t aid, server::
 						const auto & headers = adj->http.reject2(response);
 						// Если бинарные данные ответа получены
 						if(!headers.empty()){
-							
-							for(auto & header : headers)
-								cout << " ******************** " << header.first << " === " << header.second << endl;
-							
 							/**
 							 * Если включён режим отладки
 							 */
