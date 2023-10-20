e313b85c0f4d6ec48e6fc82106ec1dcf4bbf0f63
Лобарев Юрий <forman@anyks.com>
2023-10-20 21:41:36 +0300

    Отлаживаем стабильность подключения

--- sample/webClient.cpp
+++ sample/webClient.cpp
@@ -153,8 +153,8 @@ int main(int argc, char * argv[]){
 	// Создаём объект исполнителя
 	WebClient executor(&fmk, &log, &awh);
 	// Устанавливаем активный протокол подключения
-	// core.proto(awh::engine_t::proto_t::HTTP2);
-	core.proto(awh::engine_t::proto_t::HTTP1_1);
+	core.proto(awh::engine_t::proto_t::HTTP2);
+	// core.proto(awh::engine_t::proto_t::HTTP1_1);
 	// Устанавливаем название сервиса
 	log.name("WEB Client");
 	// Устанавливаем формат времени
@@ -206,8 +206,8 @@ int main(int argc, char * argv[]){
 	awh.authTypeProxy(auth_t::type_t::BASIC);
 	// awh.authTypeProxy(auth_t::type_t::DIGEST, auth_t::hash_t::MD5);
 	// Выполняем инициализацию типа авторизации
-	// awh.authType(auth_t::type_t::BASIC);
-	awh.authType(auth_t::type_t::DIGEST, auth_t::hash_t::MD5);
+	awh.authType(auth_t::type_t::BASIC);
+	// awh.authType(auth_t::type_t::DIGEST, auth_t::hash_t::MD5);
 	// Выполняем получение URL адреса сервера
 	// uri_t::url_t url = uri.parse("https://2ip.ru");
 	// uri_t::url_t url = uri.parse("https://ipv6.google.com");
