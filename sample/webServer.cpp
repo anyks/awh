4583a24fb7630e9e9f0961d71a9dc2cabe0dfc81
Лобарев Юрий <forman@anyks.com>
2023-10-27 14:04:34 +0300

    Начинаем внедрение PUSH уведомлений

--- sample/webServer.cpp
+++ sample/webServer.cpp
@@ -140,6 +140,7 @@ class WebServer {
 				"</div>\n</body>\n</html>\n";
 				// Если протокол подключения принадлежит к HTTP/2
 				if(this->_awh->proto(bid) == engine_t::proto_t::HTTP2){
+					/*
 					// Выполняем отправку заголовковй временного овтета
 					vector <pair <string, string>> headers = {
 						{":method", "GET"},
@@ -153,6 +154,7 @@ class WebServer {
 					if(this->_awh->push2(sid, bid, headers, awh::http2_t::flag_t::NONE) < 0)
 						// Если запрос не был отправлен выводим сообщение об ошибке
 						this->_log->print("Push message is not send", log_t::flag_t::WARNING);
+					*/
 					
 					// Выполняем отключение подключения
 					// this->_awh->shutdown2(bid); // +++++++++ НЕ ПОНЯТНО
