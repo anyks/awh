ec637f5d058b8688a6985ef50704856777482deb
Лобарев Юрий <forman@anyks.com>
2023-11-21 03:45:49 +0300

    Отлаживаем работу прокси-сервера

--- src/http/http2.cpp
+++ src/http/http2.cpp
@@ -813,7 +813,9 @@ ssize_t awh::Http2::send(nghttp2_session * session, const int32_t sid, uint8_t *
 	// Если буфер передаваемых данных найден
 	if(it != self->_streams.end()){
 		// Определяем размер передаваемых данных
-		result = static_cast <ssize_t> (size > (it->second.size - it->second.offset) ? ((it->second.size - it->second.offset) > 4096 ? 4096 : (it->second.size - it->second.offset)) : size);
+		result = static_cast <ssize_t> (size > (it->second.size - it->second.offset) ? (it->second.size - it->second.offset) : size);
+
+		result = (result > 4096 ? 4096 : result);
 
 		cout << " ----------------1 " << (it->second.size - it->second.offset) << " == " << size << " == " << result << endl;
 
