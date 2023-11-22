8d44be9b705f3867d7c4acbaeb2168f94155a299
Лобарев Юрий <forman@anyks.com>
2023-11-23 02:27:03 +0300

    Выполняем отладку работы прокси-сервера

--- src/server/web/http2.cpp
+++ src/server/web/http2.cpp
@@ -775,10 +775,12 @@ void awh::server::Http2::prepare(const int32_t sid, const uint64_t bid, server::
 					#endif
 					// Флаг отправляемого фрейма
 					awh::http2_t::flag_t flag = awh::http2_t::flag_t::NONE;
+					/*
 					// Если тело запроса не существует
 					if(options->http.body().empty())
 						// Устанавливаем флаг завершения потока
 						flag = awh::http2_t::flag_t::END_STREAM;
+					*/
 					// Выполняем заголовки запроса на сервер
 					const int32_t sid = web2_t::send(options->sid, bid, headers, flag);
 					// Если запрос не получилось отправить
@@ -798,6 +800,8 @@ void awh::server::Http2::prepare(const int32_t sid, const uint64_t bid, server::
 								// Выводим сообщение о выводе чанка тела
 								cout << this->_fmk->format("<chunk %zu>", entity.size()) << endl << endl;
 							#endif
+
+							cout << " ++++++++++++ " << endl;
 							/*
 							// Если нужно установить флаг закрытия потока
 							if(options->http.body().empty() && (options->http.trailers() == 0))
