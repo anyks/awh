486f6bccf3483f938f6dd75d43ffc2f51c912aa2
Лобарев Юрий <forman@anyks.com>
2023-11-25 11:11:35 +0300

    Выполняем сборку стабильной версии прокси-сервера

--- src/server/proxy.cpp
+++ src/server/proxy.cpp
@@ -654,7 +654,7 @@ void awh::server::Proxy::handshake(const int32_t sid, const uint64_t bid, const 
 								set <client::web_t::flag_t> flags = {
 									client::web_t::flag_t::ALIVE,
 									client::web_t::flag_t::NOT_STOP,
-									// client::web_t::flag_t::NOT_INFO,
+									client::web_t::flag_t::NOT_INFO,
 									client::web_t::flag_t::REDIRECTS
 								};
 								// Если флаг ожидания входящих сообщений установлен
@@ -728,7 +728,7 @@ void awh::server::Proxy::handshake(const int32_t sid, const uint64_t bid, const 
 							set <client::web_t::flag_t> flags = {
 								client::web_t::flag_t::ALIVE,
 								client::web_t::flag_t::NOT_STOP,
-								// client::web_t::flag_t::NOT_INFO,
+								client::web_t::flag_t::NOT_INFO,
 								client::web_t::flag_t::REDIRECTS
 							};
 							// Если флаг ожидания входящих сообщений установлен
