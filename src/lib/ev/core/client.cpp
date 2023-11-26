a794e5cbe153b02e16769be7a8ecd7515da249a6
Лобарев Юрий <forman@anyks.com>
2023-11-26 06:12:52 +0300

    Отлаживаем редиректы на прокси-сервере

--- src/lib/ev/core/client.cpp
+++ src/lib/ev/core/client.cpp
@@ -1355,7 +1355,7 @@ void awh::client::Core::read(const uint64_t bid) noexcept {
 							// Если запись не выполнена, входим
 							} else break;
 
-							cout << " ^^^^^^^^^^^^^^13 " << endl;
+							cout << " ^^^^^^^^^^^^^^13 " << (this->method(bid) == engine_t::method_t::READ) << endl;
 
 						// Выполняем чтение до тех пор, пока всё не прочитаем
 						} while(this->method(bid) == engine_t::method_t::READ);
