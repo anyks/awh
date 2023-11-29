a01f0e59206da3d65cc54e656d6e8c9c1c51c995
Лобарев Юрий <forman@anyks.com>
2023-11-29 05:26:30 +0300

    Выполнение оптимизации кода под потоки HTTP/2

---
+++ include/scheme/web2.hpp
@@ -0,0 +1,174 @@
+/**
+ * @file: web2.hpp
+ * @date: 2022-11-29
+ * @license: GPL-3.0
+ *
+ * @telegram: @forman
+ * @author: Yuriy Lobarev
+ * @phone: +7 (910) 983-95-90
+ * @email: forman@anyks.com
+ * @site: https://anyks.com
+ *
+ * @copyright: Copyright © 2022
+ */
+
+#ifndef __AWH_SCHEME_WEB2_SERVER__
+#define __AWH_SCHEME_WEB2_SERVER__
+
+/**
+ * Стандартная библиотека
+ */
+#include <map>
+#include <ctime>
+#include <vector>
+
+/**
+ * Наши модули
+ */
+#include <http/server.hpp>
+#include <scheme/server.hpp>
+
+// Подписываемся на стандартное пространство имён
+using namespace std;
+
+/**
+ * awh пространство имён
+ */
+namespace awh {
+	/**
+	 * server серверное пространство имён
+	 */
+	namespace server {
+		/**
+		 * scheme серверное пространство имён
+		 */
+		namespace scheme {
+			/**
+			 * WEB2 Структура схемы сети WEB/2 сервера
+			 */
+			typedef struct WEB2 : public scheme_t {
+				public:
+					/**
+					 * Stream Структура потока
+					 */
+					typedef struct Stream {
+						bool crypted;                // Флаг шифрования сообщений
+						int32_t sid;                 // Идентификатор потока
+						http_t http;                 // Создаём объект для работы с HTTP
+						http_t::compress_t compress; // Метод компрессии данных
+						/**
+						 * Stream Конструктор
+						 * @param fmk объект фреймворка
+						 * @param log объект для работы с логами
+						 */
+						Stream(const fmk_t * fmk, const log_t * log) noexcept :
+						 crypted(false), sid(1), http(fmk, log), compress(awh::http_t::compress_t::NONE) {}
+					} stream_t;
+					/**
+					 * Options Класс параметров активного клиента
+					 */
+					typedef class Options {
+						public:
+							bool alive;                                   // Флаг долгоживущего подключения
+							bool close;                                   // Флаг требования закрыть брокера
+							bool stopped;                                 // Флаг принудительной остановки
+							time_t point;                                 // Контрольная точка ответа на пинг
+							size_t requests;                              // Количество выполненных запросов
+							engine_t::proto_t proto;                      // Активный прототип интернета
+							map <int32_t, unique_ptr <stream_t>> streams; // Список активных потоков
+						public:
+							// Объект фреймворка
+							const fmk_t * fmk;
+							// Объект работы с логами
+							const log_t * log;
+						public:
+							/**
+							 * Options Конструктор
+							 * @param fmk объект фреймворка
+							 * @param log объект для работы с логами
+							 */
+							Options(const fmk_t * fmk, const log_t * log) noexcept :
+							 alive(false), close(false), stopped(false), point(0),
+							 requests(0), proto(engine_t::proto_t::HTTP1_1), fmk(fmk), log(log) {}
+						/**
+						 * ~Options Деструктор
+						 */
+						~Options() noexcept {}
+					} options_t;
+				public:
+					// Список доступных компрессоров
+					vector <awh::http_t::compress_t> compressors;
+				private:
+					// Список параметров активных клиентов
+					map <uint64_t, unique_ptr <options_t>> _options;
+				private:
+					// Создаём объект фреймворка
+					const fmk_t * _fmk;
+					// Создаём объект работы с логами
+					const log_t * _log;
+				public:
+					/**
+					 * clear Метод очистки
+					 */
+					void clear() noexcept;
+				public:
+					/**
+					 * set Метод создания параметров активного клиента
+					 * @param bid идентификатор брокера
+					 */
+					void set(const uint64_t bid) noexcept;
+					/**
+					 * rm Метод удаления параметров активного клиента
+					 * @param bid идентификатор брокера
+					 */
+					void rm(const uint64_t bid) noexcept;
+				public:
+					/**
+					 * get Метод получения параметров активного клиента
+					 * @param bid идентификатор брокера
+					 * @return    параметры активного клиента
+					 */
+					const options_t * get(const uint64_t bid) const noexcept;
+					/**
+					 * get Метод извлечения списка параметров активных клиентов
+					 * @return список параметров активных клиентов
+					 */
+					const map <uint64_t, unique_ptr <options_t>> & get() const noexcept;
+				public:
+					/**
+					 * openStream Метод открытия потока
+					 * @param sid идентификатор потока
+					 * @param bid идентификатор брокера
+					 */
+					void openStream(const int32_t sid, const uint64_t bid) noexcept;
+					/**
+					 * closeStream Метод закрытия потока
+					 * @param sid идентификатор потока
+					 * @param bid идентификатор брокера
+					 */
+					void closeStream(const int32_t sid, const uint64_t bid) noexcept;
+				public:
+					/**
+					 * getStream Метод извлечения данных потока
+					 * @param sid идентификатор потока
+					 * @param bid идентификатор брокера
+					 * @return    данные запрашиваемого потока
+					 */
+					const stream_t * getStream(const int32_t sid, const uint64_t bid) const noexcept;
+				public:
+					/**
+					 * WEB2 Конструктор
+					 * @param fmk объект фреймворка
+					 * @param log объект для работы с логами
+					 */
+					WEB2(const fmk_t * fmk, const log_t * log) noexcept : scheme_t(fmk, log), _fmk(fmk), _log(log) {}
+					/**
+					 * ~WEB2 Деструктор
+					 */
+					~WEB2() noexcept {}
+			} web2_t;
+		};
+	};
+};
+
+#endif // __AWH_SCHEME_WEB2_SERVER__
---
+++ src/scheme/web2.cpp
@@ -0,0 +1,150 @@
+/**
+ * @file: web2.cpp
+ * @date: 2023-11-29
+ * @license: GPL-3.0
+ *
+ * @telegram: @forman
+ * @author: Yuriy Lobarev
+ * @phone: +7 (910) 983-95-90
+ * @email: forman@anyks.com
+ * @site: https://anyks.com
+ *
+ * @copyright: Copyright © 2023
+ */
+
+// Подключаем заголовочный файл
+#include <scheme/web2.hpp>
+
+/**
+ * clear Метод очистки
+ */
+void awh::server::scheme::WEB2::clear() noexcept {
+	// Очищаем данные вокера
+	scheme_t::clear();
+	// Очищаем список параметров активных клиентов
+	this->_options.clear();
+	// Очищаем доступный список доступных компрессоров
+	this->compressors.clear();
+	// Освобождаем выделенную память
+	map <uint64_t, unique_ptr <options_t>> ().swap(this->_options);
+}
+/**
+ * set Метод создания параметров активного клиента
+ * @param bid идентификатор брокера
+ */
+void awh::server::scheme::WEB2::set(const uint64_t bid) noexcept {
+	// Если идентификатор брокера передан
+	if((bid > 0) && (this->_options.count(bid) < 1)){
+		// Создаём объект параметров активного клиента
+		auto ret = this->_options.emplace(bid, unique_ptr <options_t> (new options_t(this->_fmk, this->_log)));
+		// Устанавливаем контрольную точку
+		ret.first->second->point = this->_fmk->timestamp(fmk_t::stamp_t::MILLISECONDS);
+	}
+}
+/**
+ * rm Метод удаления параметров активного клиента
+ * @param bid идентификатор брокера
+ */
+void awh::server::scheme::WEB2::rm(const uint64_t bid) noexcept {
+	// Если идентификатор брокера передан
+	if((bid > 0) && !this->_options.empty()){
+		// Выполняем поиск брокера
+		auto it = this->_options.find(bid);
+		// Если брокер найден, удаляем его
+		if(it != this->_options.end())
+			// Выполняем удаление брокеров
+			this->_options.erase(it);
+	}
+}
+/**
+ * get Метод получения параметров активного клиента
+ * @param bid идентификатор брокера
+ * @return    параметры активного клиента
+ */
+const awh::server::scheme::WEB2::options_t * awh::server::scheme::WEB2::get(const uint64_t bid) const noexcept {
+	// Если идентификатор брокера передан
+	if((bid > 0) && !this->_options.empty()){
+		// Выполняем поиск брокера
+		auto it = this->_options.find(bid);
+		// Если брокер найден, выводим его параметры
+		if(it != this->_options.end())
+			// Выводим параметры подключения брокера
+			return it->second.get();
+	}
+	// Выводим результат
+	return nullptr;
+}
+/**
+ * get Метод извлечения списка параметров активных клиентов
+ * @return список параметров активных клиентов
+ */
+const map <uint64_t, unique_ptr <awh::server::scheme::WEB2::options_t>> & awh::server::scheme::WEB2::get() const noexcept {
+	// Выводим результат
+	return this->_options;
+}
+/**
+ * openStream Метод открытия потока
+ * @param sid идентификатор потока
+ * @param bid идентификатор брокера
+ */
+void awh::server::scheme::WEB2::openStream(const int32_t sid, const uint64_t bid) noexcept {
+	// Если идентификатор брокера передан
+	if((bid > 0) && (sid > -1) && !this->_options.empty()){
+		// Выполняем поиск брокера
+		auto it = this->_options.find(bid);
+		// Если брокер найден, выводим его параметры
+		if(it != this->_options.end()){
+			// Создаём объект параметров активного клиента
+			auto ret = it->second->streams.emplace(sid, unique_ptr <stream_t> (new stream_t(it->second->fmk, it->second->log)));
+			// Устанавливаем идентификатор потока
+			ret.first->second->sid = sid;
+			// Устанавливаем список доступных компрессоров
+			ret.first->second->http.compressors(this->compressors);
+		}
+	}
+}
+/**
+ * closeStream Метод закрытия потока
+ * @param sid идентификатор потока
+ * @param bid идентификатор брокера
+ */
+void awh::server::scheme::WEB2::closeStream(const int32_t sid, const uint64_t bid) noexcept {
+	// Если идентификатор брокера передан
+	if((bid > 0) && (sid > -1) && !this->_options.empty()){
+		// Выполняем поиск брокера
+		auto i = this->_options.find(bid);
+		// Если брокер найден, выводим его параметры
+		if((i != this->_options.end()) && !i->second->streams.empty()){
+			// Выполняем поиск указанного потока
+			auto j = i->second->streams.find(sid);
+			// Если поток найден
+			if(j != i->second->streams.end())
+				// Выполняем удаление потока
+				i->second->streams.erase(j);
+		}
+	}
+}
+/**
+ * getStream Метод извлечения данных потока
+ * @param sid идентификатор потока
+ * @param bid идентификатор брокера
+ * @return    данные запрашиваемого потока
+ */
+const awh::server::scheme::WEB2::stream_t * awh::server::scheme::WEB2::getStream(const int32_t sid, const uint64_t bid) const noexcept {
+	// Если идентификатор брокера передан
+	if((bid > 0) && (sid > -1) && !this->_options.empty()){
+		// Выполняем поиск брокера
+		auto i = this->_options.find(bid);
+		// Если брокер найден, выводим его параметры
+		if((i != this->_options.end()) && !i->second->streams.empty()){
+			// Выполняем поиск указанного потока
+			auto j = i->second->streams.find(sid);
+			// Если поток найден
+			if(j != i->second->streams.end())
+				// Выводим полученный результат
+				return j->second.get();
+		}
+	}
+	// Выводим результат
+	return nullptr;
+}
