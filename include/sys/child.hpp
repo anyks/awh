e9ad9e451d3dc9cff26cb2933788f9831005b435
ANYKS <forman@anyks.com>
2024-05-24 12:41:30 +0300

    Отладка работы дочерних процессов

--- include/sys/child.hpp
+++ include/sys/child.hpp
@@ -44,6 +44,7 @@ namespace awh {
 			 * Состояние очереди
 			 */
 			enum class state_t : uint8_t {
+				NONE      = 0x00, // Состояние очереди не установленно
 				INCREMENT = 0x01, // Увеличение очереди
 				DECREMENT = 0x02  // Уменьшение очереди
 			};
@@ -54,11 +55,11 @@ namespace awh {
 			// Объект дочернего потока
 			std::thread _thr;
 			// Мютекс для блокировки потока
-			mutable mutex _mtx1, _mtx2;
+			mutex _mtx1, _mtx2;
 			// Очередь полезной нагрузки
-			mutable queue <T> _payload;
+			queue <T> _payload;
 			// Условная переменная, ожидания поступления данных
-			mutable condition_variable _cv;
+			condition_variable _cv;
 		private:
 			// Функция обратного вызова которая срабатывает при передачи данных в дочерний поток
 			function <void (const T &)> _callback;
@@ -66,9 +67,34 @@ namespace awh {
 			function <void (const state_t, const size_t)> _state;
 		private:
 			/**
+			 * process Метод запуска обработки поступившей задачи
+			 */
+			void process() noexcept {
+				// Если данные в очереди существуют
+				if(!this->_payload.empty()){
+					// Извлекаем данные полезной нагрузки
+					const auto & payload = this->_payload.front();
+					// Если функция подписки на логи установлена, выводим результат
+					if(this->_callback != nullptr)
+						// Выводим сообщение лога всем подписавшимся
+						this->_callback(payload);
+					// Выполняем блокировку потока
+					this->_mtx2.lock();
+					// Удаляем текущее задание
+					this->_payload.pop();
+					// Выполняем разблокировку потока
+					this->_mtx2.unlock();
+					// Если функция обратного вызова установлена
+					if(this->_state != nullptr)
+						// Выполняем функцию обратного вызова
+						this->_state(state_t::DECREMENT, this->_payload.size());
+				}
+			}
+		private:
+			/**
 			 * receiving Метод получения данных
 			 */
-			void receiving() const noexcept {
+			void receiving() noexcept {
 				// Запускаем бесконечный цикл
 				while(!this->_stop){
 					/**
@@ -79,33 +105,22 @@ namespace awh {
 						unique_lock <mutex> lock(this->_mtx1);
 						// Выполняем ожидание на поступление новых заданий
 						this->_cv.wait_for(lock, 100ms, std::bind(&Child::checkInputData, this));
-						// Если произведена остановка выходим
-						if(this->_stop) break;
-						// Если данные в очереди существуют
-						if(!this->_payload.empty()){
-							// Извлекаем данные полезной нагрузки
-							const auto & payload = this->_payload.front();
-							// Если функция подписки на логи установлена, выводим результат
-							if(this->_callback != nullptr)
-								// Выводим сообщение лога всем подписавшимся
-								this->_callback(payload);
-							// Выполняем блокировку потока
-							this->_mtx2.lock();
-							// Удаляем текущее задание
-							this->_payload.pop();
-							// Выполняем разблокировку потока
-							this->_mtx2.unlock();
-							// Если функция обратного вызова установлена
-							if(this->_state != nullptr)
-								// Выполняем функцию обратного вызова
-								this->_state(state_t::DECREMENT, this->_payload.size());
-						}
+						// Выполняем запуск обработки поступившей задачи
+						this->process();
+						// Если произведена остановка
+						if(this->_stop)
+							// Выходим из цикла
+							break;
 					/**
 					 * Если возникает ошибка
 					 */
 					} catch(const exception & error) {
-						// Если произведена остановка выходим
-						if(this->_stop) break;
+						// Выполняем запуск обработки поступившей задачи
+						this->process();
+						// Если произведена остановка
+						if(this->_stop)
+							// Выходим из цикла
+							break;
 					}
 				}
 			}
@@ -114,7 +129,7 @@ namespace awh {
 			 * checkInputData Метод проверки на существование данных
 			 * @return результат проверки
 			 */
-			bool checkInputData() const noexcept {
+			bool checkInputData() noexcept {
 				// Если произведена остановка выходим
 				if(this->_stop)
 					// Выходим из функции
