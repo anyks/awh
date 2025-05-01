c46d8020155f69775c9274f5ad75020af6f225e7
ANYKS <forman@anyks.com>
2025-05-02 01:38:06 +0300

    Обновляем зависимости

--- include/sys/fmk.hpp
+++ include/sys/fmk.hpp
@@ -39,6 +39,7 @@
 #include <iostream>
 #include <algorithm>
 #include <type_traits>
+#include <unordered_map>
 #include <sys/types.h>
 
 /**
@@ -695,7 +696,7 @@ namespace awh {
 			 * @param escaping  символы экранирования
 			 * @return          список найденных элементов
 			 */
-			unordered_map <string, string> kv(const string & text, const string & delim, const string & separator = "=", const vector <string> & escaping = {"\""}) const noexcept;
+			unordered_map <string, string> kv(const string & text, const string & delim, const string & separator = "=", const vector <string> & escaping = {string{"\""}}) const noexcept;
 			/**
 			 * kv Метод извлечения ключей и значений из текста
 			 * @param text      текст из которого извлекаются записи
@@ -704,7 +705,7 @@ namespace awh {
 			 * @param escaping  символы экранирования
 			 * @return          список найденных элементов
 			 */
-			unordered_map <wstring, wstring> kv(const wstring & text, const wstring & delim, const wstring & separator = L"=", const vector <wstring> & escaping = {L"\""}) const noexcept;
+			unordered_map <wstring, wstring> kv(const wstring & text, const wstring & delim, const wstring & separator = L"=", const vector <wstring> & escaping = {wstring{L"\""}}) const noexcept;
 		public:
 			/**
 			 * domainZone Метод установки пользовательской зоны
