9f1158c87523df53284a0fca504309b6d25aade3
ANYKS <forman@anyks.com>
2025-11-28 06:14:33 +0300

    Реализация пингера

--- sample/server.cpp
+++ sample/server.cpp
@@ -48,7 +48,9 @@ int32_t main(int32_t argc, char * argv[]){
 	// Создаём объект асинхронного движка ввода-вывода
 	io_t io(&fmk, &log);
 	// Добавляем новое событие клиента TCP
-	event::id_t eid = io.event(event::node_t::SERVER, event::family_t::UDS, event::type_t::STREAM);
+	event::id_t eid = io.event(event::node_t::SERVER, event::family_t::IPV4, event::type_t::DATAGRAM, event::protocol_t::UDP);
+	// Устанавливаем порт события
+	io.port(eid, 2222);
 	// Инициализируем асинхронный движок ввода-вывода
 	if(io.initialize()){
 		// Устананавливаем опции события
@@ -58,7 +60,7 @@ int32_t main(int32_t argc, char * argv[]){
 		// Выводим сообщение об ошибке установки опций события
 		else cout << " Ошибка установки опций события!" << endl;
 		// Устанавливаем IP-адрес события
-		if(io.address(eid, event::address_t::UDS, "/tmp/awh.sock")){
+		if(io.address(eid, event::address_t::IPV4, "192.168.5.1")){
 			// Устанавливаем функцию обратного вызова на событие таймера
 			io.on(eid, [&log](const event::id_t eid, const event::status_t status) noexcept -> void {
 				/**
@@ -374,12 +376,8 @@ int32_t main(int32_t argc, char * argv[]){
 			io.timeout(eid, event::action_t::WRITE, 5000);
 			// Выполняем фиксацию настроек события сервера
 			if(io.commit(eid)){
-				// Если прослушивание события успешно
-				if(io.listen(eid, 100, true))
-					/**
-					 * Запускаем опрос событий
-					 */
-					while(io.poll());
+				while(true)
+					io.recv(eid);
 			}
 		// Если адрес не установлен
 		} else cout << " Ошибка установки адреса события!" << endl;
