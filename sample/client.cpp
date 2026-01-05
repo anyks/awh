45a17501b3e30db671c102e50047b1e797c878f5
ANYKS <forman@anyks.com>
2026-01-05 03:50:32 +0300

    Реализация клиент-сервера для SCTP

--- sample/client.cpp
+++ sample/client.cpp
@@ -1734,12 +1734,12 @@ int32_t main(int32_t argc, char * argv[]){
 				// Выполняем фиксацию настроек события сервера
 				if(io.commit(eid)){
 					// Если подключение к серверу прошло успешно
-					//if(io.connect(eid, true)){
+					if(io.connect(eid, true)){
 						// Выполняем запуск события
 						if(io.launch(eid)){
 							// Выводим сообщение об успешном запуске события
 							cout << " Событие успешно запущено!" << endl;
-
+							/*
 							// Текст исходящего сообщения
 							const string message("Hello from async client!");
 							// Отправляем данные обратно клиенту
@@ -1748,14 +1748,14 @@ int32_t main(int32_t argc, char * argv[]){
 								log.print("Отправлено: ID=%u, %zu байт", log_t::flag_t::INFO, eid, message.size());
 							// Если данные не отправлены
 							else log.print("Ошибка отправки: ID=%u", log_t::flag_t::CRITICAL, eid);
-
+							*/
 							/**
 							 * Запускаем опрос событий
 							 */
 							while(io.poll());
 						// Выводим сообщение об ошибке запуска события
 						} else cout << " Ошибка запуска события!" << endl;
-					//}
+					}
 				}
 			// Если адрес назначения не установлен
 			} else cout << " Ошибка установки адреса сервера!" << endl;
