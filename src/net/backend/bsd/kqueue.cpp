6f40d5d865d5164b2e3ad388d4d247b179ba2683
ANYKS <forman@anyks.com>
2026-01-06 23:04:58 +0300

    Отлаживаем работу UDS-UDP сокета

--- src/net/backend/bsd/kqueue.cpp
+++ src/net/backend/bsd/kqueue.cpp
@@ -11501,8 +11501,6 @@ bool awh::IO::commit(const event::id_t id) noexcept {
 														 * Определяем протокол
 														 */
 														switch(static_cast <uint8_t> (client->state.protocol)){
-															// Если протокол интернета не установлен
-															case static_cast <uint8_t> (event::protocol_t::NONE):
 															// Если протокол определён как TCP
 															case static_cast <uint8_t> (event::protocol_t::TCP): {
 																// Если активирован режим ручного формирования заголовков
@@ -12013,8 +12011,6 @@ bool awh::IO::commit(const event::id_t id) noexcept {
 														 * Определяем протокол
 														 */
 														switch(static_cast <uint8_t> (client->state.protocol)){
-															// Если протокол интернета не установлен
-															case static_cast <uint8_t> (event::protocol_t::NONE):
 															// Если протокол определён как TCP
 															case static_cast <uint8_t> (event::protocol_t::TCP): {
 																// Если активирован режим ручного формирования заголовков
