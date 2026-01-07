791425c88057a1038ff792114bff5b777ce4cae6
ANYKS <forman@anyks.com>
2026-01-07 23:04:29 +0300

    Добавляем поддержку аутентификации SCTP

--- sample/server.cpp
+++ sample/server.cpp
@@ -69,11 +69,11 @@ int32_t main(int32_t argc, char * argv[]){
 		io.sctpAuthenticateChunks(eid, {net::sctp::auth_chunk_t::DATA, net::sctp::auth_chunk_t::SHUTDOWN});
 		// Выполняем подписку на SCTP события
 		io.sctpEventsSubscribe(eid, {
-			net::sctp::event_type_t::EVENTS,
 			net::sctp::event_type_t::ASSOC_CHANGE,
 			net::sctp::event_type_t::SHUTDOWN_EVENT,
 			net::sctp::event_type_t::SEND_FAILED_EVENT,
-			net::sctp::event_type_t::REMOTE_ERROR
+			net::sctp::event_type_t::REMOTE_ERROR,
+			net::sctp::event_type_t::AUTHENTICATION_EVENT
 		});
 		// Устанавливаем IP-адрес события
 		if(io.address(eid, event::address_t::IPV4, "127.0.0.1")){
