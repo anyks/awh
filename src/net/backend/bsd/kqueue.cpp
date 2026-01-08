4a391656f7ff8acab1410d47ec8839a4c407a213
ANYKS <forman@anyks.com>
2026-01-08 04:50:44 +0300

    Реализация поддержки функции обратного вызова на возрождение события

--- src/net/backend/bsd/kqueue.cpp
+++ src/net/backend/bsd/kqueue.cpp
@@ -5582,19 +5582,12 @@ namespace io {
 														client->transfer.sctp.id = client->transfer.sctp.info.sinfo_assoc_id;
 														// Если функция обратного вызова для обработки информационных метаданных SCTP сообщения установлена
 														if(client->transfer.sctp.callbacks.info != nullptr){
-															
-															std::thread([client]() -> void {
-															
-															
-															
 															// Объект для хранения информационных метаданных SCTP сообщения
 															net::sctp::minfo_t minfo;
 															// Извлекаем информационные метаданные SCTP сообщения из однорангового узла
 															::sctp::info(client->transfer.sctp.info, minfo);
 															// Вызываем функцию обратного вызова для обработки информационных метаданных SCTP сообщения
-															client->transfer.sctp.callbacks.info(client->id, minfo);
-
-															}).detach();
+															client->transfer.sctp.callbacks.info(client->id, ::move(minfo));
 														}
 														// Если мы получили уведомления SCTP
 														if(client->transfer.sctp.flags & MSG_NOTIFICATION){
