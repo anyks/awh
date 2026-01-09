be8215a62b62754869e67a1fe823eaf21b87f0bb
ANYKS <forman@anyks.com>
2026-01-09 17:04:18 +0300

    Реализация нового API для работы с SCTP

--- src/net/backend/bsd/kqueue.cpp
+++ src/net/backend/bsd/kqueue.cpp
@@ -12715,7 +12715,7 @@ bool awh::IO::ControlList::clear(const event::id_t id) noexcept {
 							 */
 							#if DEBUG_MODE
 								// Выводим сообщение об ошибке
-								this->_log->debug("Blacklist does not exist for this event type", __PRETTY_FUNCTION__, std::make_tuple(id, value), log_t::flag_t::WARNING);
+								this->_log->debug("Blacklist does not exist for this event type", __PRETTY_FUNCTION__, std::make_tuple(id), log_t::flag_t::WARNING);
 							/**
 							 * Если режим отладки не включён
 							 */
