227fc58ef7fa19b4dfff22e49976259aaab53c33
ANYKS <forman@anyks.com>
2026-05-20 17:34:34 +0300

    Реализация семплов client/server SCTP

--- sample/net/sctp/client-dtls.cpp
+++ sample/net/sctp/client-dtls.cpp
@@ -426,7 +426,7 @@ int32_t main(int32_t argc, char * argv[]){
 					}
 				});
 				// Устанавливаем функцию обратного вызова на удачное подключение к серверу
-				io.on(eid, static_cast <engine::callback::connect_t> ([ctl, &tls, &io, &log](const event::id_t eid, const bool ok) noexcept -> void {
+				io.on(eid, static_cast <engine::callback::connect_t> ([ctl, &coder, &io, &log](const event::id_t eid, const bool ok) noexcept -> void {
 					// Выводим сообщение о принятии события
 					log.print("Событие подключения: ID=%u, результат: %s", log_t::flag_t::INFO, eid, ok ? "YES" : "NO");
 					// Если подключение успешно
